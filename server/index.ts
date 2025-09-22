d65e8af8fa9f7b4c6ba1b9a589b3f07b3f8fb9fe
.Tri3 <mtrigenius@gmail.com>
2025-09-22 23:41:00 +0700

    second cm

--- server/index.ts
+++ server/index.ts
@@ -21,7 +21,7 @@ routes(app);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req: Request, res: Response) => {
+  app.get("/*", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
